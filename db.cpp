1d724ad6c72c5cd2f30557a67736bb0620c713db
Apurva Patil <95519627+appu-11@users.noreply.github.com>
2022-06-27 20:11:09 +0530

    Add files via upload

---
+++ db.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr<<#x<<" ";_print_(x);cerr<<endl;
+#else
+#define dbg(x)
+#endif
+
+void _print_(ll t) { cerr << t; }
+void _print_(int t) { cerr << t; }
+void _print_(string t) { cerr << t; }
+void _print_(char t) { cerr << t; }
+void _print_(long double t) { cerr << t; }
+void _print_(double t) { cerr << t; }
+
+template <class T, class V> void _print_(pair <T, V> p);
+template <class T, class V> void _print_(pair <T, V> p) { cerr << "{"; _print_(p.first); cerr << ","; _print_(p.second); cerr << "}"; }
+template <class T> void _print_(set <T> v);
+template <class T> void _print_(set <T> v) { cerr << "[ "; for (T i : v) { _print_(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print_(map <T, V> v);
+template <class T, class V> void _print_(map <T, V> v) { cerr << "[ "; for (auto i : v) { _print_(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print_(multiset <T> v);
+template <class T> void _print_(multiset <T> v) { cerr << "[ "; for (T i : v) { _print_(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print_(vector <T> v);
+template <class T> void _print_(vector <T> v) { cerr << "[ "; for (T i : v) { _print_(i); cerr << " "; } cerr << "]"; }
